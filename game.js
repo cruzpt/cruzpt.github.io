ec4edf2ff6419c3353b4d67ed5898e699c568811
Pedro Cruz <cruzpt@gmail.com>
2021-05-10 22:15:54 +0100

    mobile browsers
    Add detect mobile browsers

--- game.js
+++ game.js
@@ -12,13 +12,42 @@ var started = false;
 //game level
 var level = 0;
 
-$(document).keypress(function(){
+
+
+//$(".btnmob").hide();
+$(".btnmob").css("display", "none");
+
+//$("#id").css("display", "block");
+
+var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+if (isMobile) {
+  console.log("is mobile");
+  //$("#level-title").hide();
+  //$(".btnmob").show();
+
+  $("#level-title").text("On mobile devices click button to start")
+  $(".btnmob").css("display", "block");
+
+  $(".btnmob").click(function() {
+    if (!started) {
+      $("#level-title").text("Level " + level);
+      nextSequence();
+      started = true;
+    }
+  });
+}
+
+
+$(document).keypress(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
     started = true;
   }
 });
+
+
+
 
 
 
@@ -30,7 +59,7 @@ $(".btn").click(function() {
   //contents of the variable userChosenColour
   userClickedPattern.push(userChosenColour);
   animate(userChosenColour);
-  checkAnswer(userClickedPattern.length -1);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
 
@@ -38,21 +67,27 @@ $(".btn").click(function() {
 function checkAnswer(currentLevel) {
   if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
     //console.log("sucess");
-    if(userClickedPattern.length === gamePattern.length) {
+    if (userClickedPattern.length === gamePattern.length) {
       setTimeout(function() {
         nextSequence();
       }, 1000);
     }
-  }else {
+  } else {
     //console.log("wrong");
     playSound("wrong");
     playSound("wrong2");
     $("body").addClass("game-over");
-    setTimeout(function(){
-        $("body").removeClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
 
     }, 200);
-    $("#level-title").text("Trump Over, press any Key to Restart the Game");
+    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    if (isMobile) {
+      $("#level-title").text("Trump Over, click button to restart");
+    } else{
+      $("#level-title").text("Trump Over, press any Key to Restart the Game");
+    }
+
     startOver();
 
   };
@@ -69,24 +104,25 @@ function nextSequence() {
   var randomChosenColour = buttonColours[randomNumber];
   //randomChosenColour generated in step 4 to the end of the gamePattern
   gamePattern.push(randomChosenColour);
-//animate a flash to the button with the same id as the randomChosenColour
+  //animate a flash to the button with the same id as the randomChosenColour
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-//play the sound for the button colour selected
+  //play the sound for the button colour selected
   playSound(randomChosenColour);
 }
 
 function playSound(name) {
   //play the sound for the button colour selected
-    var audio = new Audio("sounds/" + name + ".mp3");
-    audio.play();
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
 
-function animate(currentColour) {
-    $("#" + currentColour).addClass("pressed");
-    setTimeout(function(){
-        $("#" + currentColour).removeClass("pressed");
-    }, 100);
+function animate(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
 }
+
 function startOver() {
   level = 0;
   gamePattern = [];
